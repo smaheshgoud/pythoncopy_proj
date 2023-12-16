30e9c37a4a537dff83a4cb567126c922e864f4dc
smaheshgoud <48258872+smaheshgoud@users.noreply.github.com>
2023-12-15 22:35:09 -0600

    Update script_db.py

--- script_db.py
+++ script_db.py
@@ -9,10 +9,14 @@ def connect_to_database(username, password):
     # Construct the connection string
     connection_string = f'DRIVER=SQL Server;SERVER={server};DATABASE={database};UID={username};PWD={password}'
 
-    # Establish the database connection
-    connection = pyodbc.connect(connection_string)
-
-    return connection
+    try:
+        # Establish the database connection
+        connection = pyodbc.connect(connection_string)
+        print('Connected to the database successfully.')
+        return connection
+    except Exception as e:
+        print(f'Error connecting to the database: {e}')
+        sys.exit(1)
 
 def execute_query(connection):
     cursor = connection.cursor()
@@ -20,20 +24,28 @@ def execute_query(connection):
     # Replace this query with your actual SQL query
     query = 'SELECT * FROM your_table_name'
 
-    cursor.execute(query)
+    try:
+        cursor.execute(query)
+        print('Query executed successfully. Results:')
+        
+        # Fetch all the rows from the query result
+        rows = cursor.fetchall()
 
-    # Fetch all the rows from the query result
-    rows = cursor.fetchall()
-
-    # Display the results
-    for row in rows:
-        print(row)
+        # Display the results
+        for row in rows:
+            print(row)
+    except Exception as e:
+        print(f'Error executing the query: {e}')
+    finally:
+        cursor.close()
 
 if __name__ == "__main__":
     # Retrieve database credentials from command-line arguments
     db_username = sys.argv[1]
     db_password = sys.argv[2]
 
+    print(f'Database Username: {db_username}')
+    
     # Connect to the database
     db_connection = connect_to_database(db_username, db_password)
 
@@ -43,3 +55,4 @@ if __name__ == "__main__":
     finally:
         # Close the database connection
         db_connection.close()
+        print('Database connection closed.')
