8184c7f69027be7d2f6adf242058ed0e10fd1662
smaheshgoud <48258872+smaheshgoud@users.noreply.github.com>
2024-03-12 17:51:17 -0500

    Update download_script1.ps1

--- JAMF/download_script1.ps1
+++ JAMF/download_script1.ps1
@@ -1,3 +1,4 @@
+#########sh
 ACCESS_TOKEN="YOUR_ACCESS_TOKEN"
 JAMF_URL="https://your-jamf-pro-instance"
 POLICIES_ENDPOINT="$JAMF_URL/JSSResource/policies"
@@ -5,7 +6,7 @@ POLICIES_ENDPOINT="$JAMF_URL/JSSResource/policies"
 curl -X GET \
   -H "Authorization: Bearer $ACCESS_TOKEN" \
   $POLICIES_ENDPOINT
-####################
+####################sh
 
 ACCESS_TOKEN="YOUR_ACCESS_TOKEN"
 JAMF_URL="https://your-jamf-pro-instance"
@@ -14,3 +15,24 @@ PACKAGES_ENDPOINT="$JAMF_URL/JSSResource/packages"
 curl -X GET \
   -H "Authorization: Bearer $ACCESS_TOKEN" \
   $PACKAGES_ENDPOINT
+###################ps1
+$ACCESS_TOKEN = "YOUR_ACCESS_TOKEN"
+$JAMF_URL = "https://your-jamf-pro-instance"
+
+# List Policies
+$POLICIES_ENDPOINT = "$JAMF_URL/JSSResource/policies"
+$headers = @{
+    Authorization = "Bearer $ACCESS_TOKEN"
+}
+$responsePolicies = Invoke-RestMethod -Uri $POLICIES_ENDPOINT -Headers $headers -Method Get
+
+# List Packages
+$PACKAGES_ENDPOINT = "$JAMF_URL/JSSResource/packages"
+$responsePackages = Invoke-RestMethod -Uri $PACKAGES_ENDPOINT -Headers $headers -Method Get
+
+# Output responses
+Write-Host "Policies:"
+Write-Output $responsePolicies
+
+Write-Host "Packages:"
+Write-Output $responsePackages
